de5a148b9fa0add22580c945ce32742070df7c8b
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-11-16 19:45:54 -0600

    add:perfildeusuario

--- src/Componentes/Paciente/UserProfile.jsx
+++ src/Componentes/Paciente/UserProfile.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Box, CircularProgress, Button, TextField, Grid, Alert, useTheme } from '@mui/material';
+import { Container, Typography, Box, CircularProgress, Button, TextField, Grid, Alert, useTheme, IconButton, LinearProgress, Snackbar } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import axios from 'axios';
+import zxcvbn from 'zxcvbn';
 
 const UserProfile = () => {
   const theme = useTheme(); // Usamos el tema actual para los estilos
@@ -16,6 +18,9 @@ const UserProfile = () => {
   const [csrfToken, setCsrfToken] = useState(''); // Token CSRF
   const [updateError, setUpdateError] = useState(null);
   const [updateSuccess, setUpdateSuccess] = useState(null);
+  const [showPassword, setShowPassword] = useState(false); // Estado para mostrar/ocultar la contraseña
+  const [passwordStrength, setPasswordStrength] = useState(0); // Estado para la fuerza de la contraseña
+  const [openSnackbar, setOpenSnackbar] = useState(false); // Control para mostrar el Snackbar
 
   // Función para obtener los datos del perfil del usuario
   const fetchUserProfile = async () => {
@@ -45,19 +50,20 @@ const UserProfile = () => {
     event.preventDefault();
     setUpdateError(null);
     setUpdateSuccess(null);
-  
+    setOpenSnackbar(true);
+
     if (newPassword !== confirmPassword) {
       setUpdateError('Las nuevas contraseñas no coinciden.');
       return;
     }
-  
+
     // Validar la nueva contraseña (al menos una mayúscula, un número, un signo y mínimo 8 caracteres)
     const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
     if (!passwordRegex.test(newPassword)) {
       setUpdateError('La nueva contraseña debe contener al menos una letra mayúscula, un número, un signo especial y ser de mínimo 8 caracteres.');
       return;
     }
-  
+
     try {
       const response = await axios.post(
         'http://localhost:4000/api/modificar-contrasena',
@@ -74,7 +80,7 @@ const UserProfile = () => {
           },
         }
       );
-  
+
       if (response.status === 200) {
         setUpdateSuccess(response.data.message);
         setUpdateError(null);
@@ -83,7 +89,6 @@ const UserProfile = () => {
         setConfirmPassword('');
       }
     } catch (error) {
-      // Manejar el error de forma segura verificando que response esté definido
       if (error.response && error.response.data && error.response.data.message) {
         setUpdateError(error.response.data.message);
       } else {
@@ -92,7 +97,31 @@ const UserProfile = () => {
       setUpdateSuccess(null);
     }
   };
-  
+
+  // Función para manejar el cambio de la contraseña y calcular su fortaleza
+  const handlePasswordChange = (e) => {
+    const pwd = e.target.value;
+    setNewPassword(pwd);
+    const strength = zxcvbn(pwd).score;
+    setPasswordStrength(strength);
+  };
+
+  // Alternar visibilidad de la contraseña
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  // Obtener el mensaje de fortaleza de la contraseña basado en el puntaje
+  const getPasswordStrengthMessage = (score) => {
+    switch (score) {
+      case 0: return "Muy débil";
+      case 1: return "Débil";
+      case 2: return "Aceptable";
+      case 3: return "Fuerte";
+      case 4: return "Muy fuerte";
+      default: return "";
+    }
+  };
 
   useEffect(() => {
     fetchUserProfile();
@@ -180,9 +209,6 @@ const UserProfile = () => {
         {/* Mostrar el formulario solo si showUpdatePasswordForm es true */}
         {showUpdatePasswordForm && (
           <Box sx={{ marginTop: '2rem' }}>
-            {updateError && <Alert severity="error">{updateError}</Alert>}
-            {updateSuccess && <Alert severity="success">{updateSuccess}</Alert>}
-
             <form onSubmit={handleUpdatePassword}>
               <TextField
                 label="Contraseña Actual"
@@ -196,24 +222,51 @@ const UserProfile = () => {
               />
               <TextField
                 label="Nueva Contraseña"
-                type="password"
+                type={showPassword ? "text" : "password"}
                 value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
+                onChange={handlePasswordChange}
                 fullWidth
                 margin="normal"
                 required
                 sx={{ marginBottom: '1.5rem' }}
+                InputProps={{
+                  endAdornment: (
+                    <IconButton onClick={toggleShowPassword}>
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  )
+                }}
               />
               <TextField
                 label="Confirmar Nueva Contraseña"
-                type="password"
+                type={showPassword ? "text" : "password"}
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 fullWidth
                 margin="normal"
                 required
                 sx={{ marginBottom: '1.5rem' }}
+                InputProps={{
+                  endAdornment: (
+                    <IconButton onClick={toggleShowPassword}>
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  )
+                }}
               />
+
+              {/* Barra de fortaleza de la contraseña */}
+              <Box display="flex" alignItems="center" mt={2}>
+                <Typography variant="body2" color="textSecondary" style={{ marginLeft: '10px' }}>
+                  Fortaleza de la contraseña: {getPasswordStrengthMessage(passwordStrength)}
+                </Typography>
+              </Box>
+              <LinearProgress
+                variant="determinate"
+                value={(passwordStrength + 1) * 20}  // La barra de progreso según la fortaleza
+                sx={{ marginTop: '10px', marginBottom: '10px' }}
+              />
+
               <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '1rem' }}>
                 <Button
                   type="submit"
@@ -228,6 +281,13 @@ const UserProfile = () => {
           </Box>
         )}
       </Box>
+
+      {/* Snackbar para mostrar éxito o error en la esquina inferior izquierda */}
+      <Snackbar open={openSnackbar} autoHideDuration={3000} onClose={() => setOpenSnackbar(false)} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
+        <Alert severity={updateSuccess ? "success" : "error"} onClose={() => setOpenSnackbar(false)} sx={{ width: '100%' }}>
+          {updateSuccess || updateError}
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
